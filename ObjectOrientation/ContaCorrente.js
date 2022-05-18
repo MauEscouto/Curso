19b8fbd9e9452d2ab8ad19e4027bef466ab09f57
Mauricio Escouto Melo <103608018+MauricioEscouto@users.noreply.github.com>
2022-05-18 19:59:49 -0300

    Update ContaCorrente.js

--- ObjectOrientation/ContaCorrente.js
+++ ObjectOrientation/ContaCorrente.js
@@ -1,16 +1,33 @@
+import{Cliente} from "./Cliente.js"
 export class ContaCorrente{
     agencia;
-    cliente;
-    saldo;
+    _cliente;
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+           this._cliente = novoValor; 
+        }
+    }
+    get cliente(){
+        return this._cliente;
+    }
+    
+    _saldo = 0;
+    get saldo(){
+        return this._saldo;
+    }
 
     sacar(valor){
-        if(this.saldo >= valor)
-            this.saldo -= valor;
+        if(this._saldo >= valor)
+            this._saldo -= valor;
             return valor;
     }
     depositar(valor){
-        if(valor <= 0)return; //se o if tem só uma linha, nao precisa de chaves
-        this.saldo += valor;    // se o valor for menor que 0, executa o return que já para a função
+        if(valor <= 0){
+           return;  
+        }
+        this._saldo += valor;         
+        //se o if tem só uma linha, nao precisa de chaves
+            // se o valor for menor que 0, executa o return que já para a função
     }
     transferir(valor, conta){
         const ValorSacado = this.sacar(valor);
