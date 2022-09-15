9134f4d1d2eaceb9453aa82cfb3b86111b75fecf
Mauricio Escouto Melo <103608018+MauricioEscouto@users.noreply.github.com>
2022-09-15 00:10:15 -0300

    continuando curso de JS para web

--- JS-para-web/paginas-dinamicas/main.js
+++ JS-para-web/paginas-dinamicas/main.js
@@ -1,1 +1,28 @@
-document.querySelector('.tecla_pom');
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+let contador = 0;
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla =  listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const audio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(audio);
+    }
+
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
+
